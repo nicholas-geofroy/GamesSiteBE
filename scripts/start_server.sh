394b2f20221ca565eb0d8f073018f09363aa7dd5
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-14 18:32:18 -0400

    spawn server in background

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -14,8 +14,13 @@ echo "$artifacts_dir"
 pushd $artifacts_dir
 
 play_secret=$(aws ssm get-parameters --region us-east-2 --names PLAY_HTTP_SECRET_KEY --with-decryption --query Parameters[0].Value --output text)
-unzip "target/universal/cardsite-1.0-SNAPSHOT.zip"
+unzip -o "target/universal/cardsite-1.0-SNAPSHOT.zip"
 chmod +x cardsite-1.0-SNAPSHOT/bin/cardsite
-cardsite-1.0-SNAPSHOT/bin/cardsite -Dplay.http.secret.key=$play_secret
+
+echo "Play Secret: $play_secret"
+cardsite-1.0-SNAPSHOT/bin/cardsite -Dplay.http.secret.key=$play_secret &
+GAMESITE_PID=$!
+
+echo "Spawned gamesite process, PID: $GAMESITE_PID"
 
 popd
