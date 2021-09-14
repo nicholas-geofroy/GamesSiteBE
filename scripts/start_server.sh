aa7cf81d1ab7ade9201b54f2761c5851f422465b
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-14 17:47:27 -0400

    use openjdk

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,14 +4,14 @@ echo "start scala server"
 current_dir=$(pwd)
 echo "$current_dir"
 
-artifacts_dir="$current_dir/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
-echo "Artifacts Dir:"
+archive_dir="$current_dir/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
+echo "Archive Dir:"
+echo "$archive_dir"
+
+artifacts_dir="/var/cardsite"
+echo "Cardsite Dir"
 echo "$artifacts_dir"
 pushd $artifacts_dir
-
-echo "Cardsite Dir Files"
-tmp=$(ls -l /var/cardsite)
-echo "$tmp"
 
 play_secret=$(aws ssm get-parameters --region us-east-2 --names PLAY_HTTP_SECRET_KEY --with-decryption --query Parameters[0].Value --output text)
 unzip "target/universal/cardsite-1.0-SNAPSHOT.zip"
