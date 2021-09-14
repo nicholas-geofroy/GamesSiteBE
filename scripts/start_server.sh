4ffab03d55d4a9fc5294758b872786aa5b047667
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-14 16:59:44 -0400

    debug logs

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "start scala server"
+current_dir=$(pwd)
+echo "$current_dir"
+files=$(ls -l)
+echo "Files:"
+echo "$files"
 play_secret=$(aws ssm get-parameters --region us-east-2 --names PLAY_HTTP_SECRET_KEY --with-decryption --query Parameters[0].Value --output text)
 unzip target/universal/cardsite-1.0-SNAPSHOT.zip
 chmod +x cardsite-1.0-SNAPSHOT/bin/cardsite
