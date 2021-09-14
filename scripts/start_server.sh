893463d15a9e498f099d7717886e79fde8b43fc4
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-14 16:43:46 -0400

    start play server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "start scala server"
-ls -a
+play_secret=$(aws ssm get-parameters --region us-east-2 --names PLAY_HTTP_SECRET_KEY --with-decryption --query Parameters[0].Value --output text)
+unzip target/universal/cardsite-1.0-SNAPSHOT.zip
+chmod +x cardsite-1.0-SNAPSHOT/bin/cardsite
+cardsite-1.0-SNAPSHOT/bin/cardsite -Dplay.http.secret.key=$play_secret
