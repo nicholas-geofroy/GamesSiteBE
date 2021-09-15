d87e876f6f4ccdf508a8b7456be09c4caba1c3a2
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-14 20:36:12 -0400

    fix typo in env var

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -4,8 +4,8 @@ set -e
 echo "Stop App"
 server_pid_file="/var/cardsite/cardsite-1.0-SNAPSHOT/RUNNING_PID"
 
-if [ -f "$server_pid_fil" ]; then
-    echo "$server_pid_fil exists. killing and deleting"
+if [ -f "$server_pid_file" ]; then
+    echo "$server_pid_file exists. killing and deleting"
     server_pid=$(cat $server_pid_file)
     kill server_pid
     rm $server_pid_file
