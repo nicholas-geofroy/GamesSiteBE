f8004c4a609446cd158696a3a8fe25d1644adbe4
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-15 21:40:19 -0400

    remove unneded debug echos and quote values

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -2,23 +2,13 @@
 
 set -e
 
-function setupEnvVar() {
-  key=$1
-  val=$2
-  export $key=$val
-}
-
 echo "setup env vars"
 params_json=$(aws ssm get-parameters-by-path --path / --region us-east-2 --with-decryption --no-paginate --query "Parameters[*].{Name:Name,Value:Value}")
-echo "Params JSON:"
-echo "$params_json"
 
 IFS=$'\n'
 for encoded_row in $(echo "$params_json" | jq -r '.[] | @base64'); do
   row=$(base64 --decode <<< $encoded_row)
-  echo "row: $row"
   env_var_name=$(jq -r '.Name' <<< $row)
   env_var_val=$(jq -r '.Value' <<< $row)
-  echo "set $env_var_name=$env_var_val"
-  export $env_var_name=$env_var_val
+  export $env_var_name="$env_var_val"
 done
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,12 +4,6 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "start scala server"
-current_dir=$(pwd)
-echo "$current_dir"
-
-archive_dir="$current_dir/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
-echo "Archive Dir:"
-echo "$archive_dir"
 
 artifacts_dir="/var/cardsite"
 echo "Cardsite Dir"
@@ -17,8 +11,6 @@ echo "$artifacts_dir"
 pushd $artifacts_dir
 
 source "$SCRIPT_DIR/setup_env.sh"
-
-echo "play http secret key $PLAY_HTTP_SECRET_KEY"
 
 unzip -o -q "target/universal/cardsite-1.0-SNAPSHOT.zip"
 chmod +x cardsite-1.0-SNAPSHOT/bin/cardsite
