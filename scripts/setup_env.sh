e14ab2947aa583471c5bd72b5d7f009b2ae95899
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-15 21:15:37 -0400

    debug loggs to test setup env

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -10,6 +10,8 @@ function setupEnvVar() {
 
 echo "setup env vars"
 params_json=$(aws ssm get-parameters-by-path --path / --region us-east-2 --with-decryption --no-paginate --query "Parameters[*].{Name:Name,Value:Value}")
+echo "Params JSON:"
+echo "$params_json"
 
 IFS=$'\n'
 for row in $(echo "$params_json" | jq -c '. | map([.Name, .Value])' | jq @sh)
