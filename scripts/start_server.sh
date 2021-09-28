556fc5f3cc3ee7e20e2354a62e00aa61fa148288
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-27 21:17:26 -0400

    use rsync so it will overwrite on app copy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -14,7 +14,7 @@ source "$SCRIPT_DIR/setup_env.sh"
 
 unzip -o -q "target/universal/cardsite-1.0-SNAPSHOT.zip"
 chmod +x cardsite-1.0-SNAPSHOT/bin/cardsite
-mv app cardsite-1.0-SNAPSHOT/app
+rsync -a app cardsite-1.0-SNAPSHOT/app
 
 nohup cpulimit -l 30 -- cardsite-1.0-SNAPSHOT/bin/cardsite \
   -Dplay.http.secret.key=$PLAY_HTTP_SECRET_KEY \
